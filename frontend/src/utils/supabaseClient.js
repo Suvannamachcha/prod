10f2a379c4baff0ff252f8dccdd7fdc55888550d
Suvannamachcha <suvannamachcha@gmail.com>
2024-11-19 14:44:18 +0530

    Update supabaseClient.js

--- frontend/src/utils/supabaseClient.js
+++ frontend/src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 // /frontend/src/utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://<your-project-id>.supabase.co';
-const supabaseAnonKey = '<your-anon-key>';
+const supabaseUrl = process.env.'https://oxclvnkiiaquhujbdnjt.supabase.co';
+const supabaseAnonKey = process.env.'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94Y2x2bmtpaWFxdWh1amJkbmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5NDM4NDEsImV4cCI6MjA0NzUxOTg0MX0.4V4WNwq8ktDKP8xmbOFgrO_emDBkITpfwk3H9IlKPv0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
